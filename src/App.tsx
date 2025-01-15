874130dc9074d77fb60d8838bdae4ea7a526dc3f
Singithi Kandage <singithi.kandage@gmail.com>
2025-01-15 14:07:20 -0500

    path aliasing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "@/pages/HomePage/HomePage";
+import AboutPage from "@/pages/AboutPage/AboutPage";
+import ServicesPage from "@/pages/ServicesPage/ServicesPage";
+import PrivacyPolicyPage from "@/pages/PrivacyPolicyPage/PrivacyPolicyPage";
+import TermsConditionsPage from "@/pages/TermsConditionsPage/TermsConditionsPage";
+
+// styles
 import "./App.scss";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+        <Route path="/term-and-conditions" element={<TermsConditionsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { AliasOptions, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+//@ts-ignore
+import path from "path";
+
+//@ts-ignore
+const root = path.resolve(__dirname, "src");
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": root,
+    } as AliasOptions,
+  },
+});
