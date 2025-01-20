1a330fd2502bfc985ea36aed0e0299c8551c56f0
Singithi Kandage <singithi.kandage@gmail.com>
2025-01-20 11:34:30 -0500

    added about page

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -2,9 +2,39 @@ import "./AboutPage.scss";
 
 const AboutPage = () => {
   return (
-    <section className="section about-section">
-      <h1>About</h1>
-    </section>
+    <div className="page">
+      <section className="section about-section">
+        <div className="section-content">
+          <h1>About</h1>
+
+          <p>
+            I am a astrologer with a passion for helping people gain insight
+            into their lives through the ancient science of astrology. I have
+            been practicing Western archetypal astrology for several years, and
+            during this time, I have helped countless individuals find
+            direction, purpose, and meaning in their lives.
+          </p>
+          <p>
+            My approach to astrology combines the psychological approach to
+            Western astrology, which I blend with the ancient techniques of
+            Vedic teaching to offer a comprehensive and personalized reading to
+            each of my clients. Whether you are seeking guidance on love and
+            relationships, career and finances, health and wellness, or personal
+            growth and spirituality, I am here to help.
+          </p>
+          <p>
+            My services include birth chart readings, synastry and composite
+            chart analysis, and transit astrology. Currently, I only provide my
+            services in English.
+          </p>
+          <p>
+            Thank you for considering me for your astrological needs. I look
+            forward to helping you gain clarity and insight into your life
+            through the transformative power of astrology.
+          </p>
+        </div>
+      </section>
+    </div>
   );
 };
 
